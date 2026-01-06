314f4e639c8da58d7d087297e78e2a063edab8ba
Tiago Silveira <tiagojesusilveira@gmail.com>
2026-01-06 16:39:05 +0100

    Exerise 0

---
+++ ex00/includes/whatever.hpp
@@ -0,0 +1,37 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+#include <iostream>
+#include <iomanip>
+#include <ostream>
+#include <limits>
+#include <cmath>
+
+template<typename T>
+void	swap(T elem1, T elem2)
+{
+	T temp;
+
+	temp = elem1;
+	elem1 = elem2;
+	elem2 = temp;	
+}
+
+template<typename T>
+T	min(T elem1, T elem2)
+{
+	if (elem1 < elem2)
+		return (elem1);
+	else
+		return (elem2);
+}
+
+template<typename T>
+T	max(T elem1, T elem2)
+{
+	if (elem1 > elem2)
+		return (elem1);
+	else
+		return (elem2);
+}
+
+#endif
